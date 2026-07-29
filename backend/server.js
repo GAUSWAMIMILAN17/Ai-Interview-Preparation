f71358a8ff8abd2aa0ae67722dba29e918db9553
Milan Gauswami <your-github-email@example.com>
2026-07-29 15:59:33 +0530

    cors

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,7 @@ const app = express();
 //middleware to handle CORS
 app.use(
   cors({
-    origin: ["http://localhost:5173", process.env.FRONTEND_URL], // React frontend URL
+    origin: [process.env.FRONTEND_URL], // React frontend URL "http://localhost:5173", 
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
