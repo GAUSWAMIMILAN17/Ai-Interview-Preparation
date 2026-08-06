302cd3993631058ef1993ca90b3430feb894cc7b
Milan Gauswami <your-github-email@example.com>
2026-08-06 21:40:52 +0530

    Finel Cors

--- backend/server.js
+++ backend/server.js
@@ -22,8 +22,8 @@ const app = express();
 //middleware to handle CORS
 app.use(
   cors({
-    // origin: process.env.FRONTEND_URL, // React frontend URL "http://localhost:5173",
-        origin:"http://localhost:5173",
+    origin: process.env.FRONTEND_URL, // React frontend URL "http://localhost:5173",
+        // origin:"http://localhost:5173",
 
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
